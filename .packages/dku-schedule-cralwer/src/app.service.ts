7d8cffd45fac3ec00f9b7871cf95f53ba1151b3a
JunilHwang <junil.h@kakao.com>
2022-01-19 08:25:57 +0900

    feat: 과목 정보를 읽어서 data 폴더에 저장하는 작업 완료

--- .packages/dku-schedule-cralwer/src/app.service.ts
+++ .packages/dku-schedule-cralwer/src/app.service.ts
@@ -1,11 +1,85 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
-import { ScheduleResponse } from './ScheduleTypes';
+import { Lecture, ScheduleResponse } from './ScheduleTypes';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const headers = {
   'content-type': 'application/x-www-form-urlencoded',
   accept: 'application/json',
 };
+
+const enum LectureType {
+  LIBERAL_ARTS = 'liberal-arts',
+  MAJORS = 'majors',
+}
+
+interface PayloadBody {
+  yy: number;
+  semCd: 1 | 2;
+  qrySxn: 1 | 2;
+  lesnPlcCd: 1 | 2;
+}
+
+function getPayloadKey({ yy, semCd, qrySxn, lesnPlcCd }: PayloadBody) {
+  const campus = lesnPlcCd === 1 ? 'Jukjeon' : 'Cheonan';
+  const lectureTypes = [
+    undefined,
+    LectureType.MAJORS,
+    LectureType.LIBERAL_ARTS,
+  ];
+  return `${String(yy).substr(-2)}-${semCd}-${
+    lectureTypes[qrySxn]
+  }-${campus}.json`;
+}
+
+function parsePayloadToMap(
+  payloads: PayloadBody[],
+): Record<string, PayloadBody> {
+  return payloads.reduce((obj, payload) => {
+    obj[getPayloadKey(payload)] = payload;
+    return obj;
+  }, {});
+}
+
+const payloadMap = parsePayloadToMap([
+  {
+    yy: 2021,
+    semCd: 2,
+    qrySxn: 1,
+    lesnPlcCd: 1,
+  },
+  {
+    yy: 2021,
+    semCd: 2,
+    qrySxn: 1,
+    lesnPlcCd: 2,
+  },
+  {
+    yy: 2021,
+    semCd: 2,
+    qrySxn: 2,
+    lesnPlcCd: 1,
+  },
+  {
+    yy: 2021,
+    semCd: 2,
+    qrySxn: 2,
+    lesnPlcCd: 2,
+  },
+  {
+    yy: 2022,
+    semCd: 1,
+    qrySxn: 1,
+    lesnPlcCd: 1,
+  },
+  {
+    yy: 2022,
+    semCd: 1,
+    qrySxn: 1,
+    lesnPlcCd: 2,
+  },
+]);
 
 @Injectable()
 export class AppService {
@@ -14,39 +88,38 @@ export class AppService {
   constructor() {
     const scheduleClient = axios.create({
       baseURL: 'https://webinfo.dankook.ac.kr',
-    })
+    });
     scheduleClient.interceptors.response.use(({ data }) => data);
 
     this.scheduleClient = scheduleClient;
   }
 
-  getHello(): string {
-    return 'Hello World!';
+  public async saveSchedule() {
+    const dataPath = path.join(process.env.INIT_CWD, '../../data');
+
+    for (const [filename, payload] of Object.entries(payloadMap)) {
+      const lectures: Lecture[] = await this.fetchSchedule(payload);
+      fs.writeFileSync(
+        `${dataPath}/${filename}`,
+        JSON.stringify(lectures),
+        'utf-8',
+      );
+    }
   }
 
-  public async fetchSchedule() {
-    const payload = Object.entries({
-      yy: 2021,
-      semCd: 2,
-      qrySxn: 2,
-      curiCparCd: null,
-      lesnPlcCd: 1,
-      mjSubjKnm: null,
-      mjDowCd: null,
-      grade: null,
-      pfltNm: null,
-    })
-      .map(([k, v]) => `${k}=${v || ''}`)
+  public async fetchSchedule(payload: PayloadBody): Promise<Lecture[]> {
+    const { scheduleClient } = this;
+    const url = '/tiac/univ/lssn/lpci/views/lssnPopup/tmtbl.do';
+    const data = Object.entries(payload)
+      .map(([k, v]) => `${k}=${v}`)
       .join('&');
-    const url =
-      '/tiac/univ/lssn/lpci/views/lssnPopup/tmtbl.do';
-    const data: ScheduleResponse = await this.scheduleClient.post(url, payload, { headers });
-    const { lctTmtblDscMjList } = data;
+    const config = { headers };
 
-    const keys = Object.keys(lctTmtblDscMjList[0]);
-
-    for (const key of Object.values(keys)) {
-      console.log(key, [...new Set(lctTmtblDscMjList.map((v) => v[key]))]);
-    }
+    const response: ScheduleResponse = await scheduleClient.post(
+      url,
+      data,
+      config,
+    );
+    return response.lctTmtblDscMjList;
   }
 }
--- .packages/dku-schedule-cralwer/src/main.ts
+++ .packages/dku-schedule-cralwer/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {AppService} from "./app.service";
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const appService: AppService = app.get(AppService);
 
-  appService.fetchSchedule();
+  appService.saveSchedule();
 
   await app.close();
 }
