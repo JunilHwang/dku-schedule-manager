437941ca0aafc6b4116507e8157a13bfb139e547
junilHwang <junil.h@kakao.com>
2022-01-19 21:46:04 +0900

    feat: element ui 추가

--- .packages/dku-schedule-client/src/main.ts
+++ .packages/dku-schedule-client/src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 import "./styles/reset.scss";
+
+import ko from "element-plus/es/locale/lang/ko";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+
+createApp(App)
+  .use(ElementPlus, {
+    locale: ko,
+  })
+  .mount("#app", true);
