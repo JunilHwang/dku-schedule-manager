467161d1e363f9adde3bc97d37634ac8a346ede2
JunilHwang <junil.h@kakao.com>
2022-01-21 09:19:05 +0900

    build

--- .packages/dku-schedule-client/src/router/index.ts
+++ .packages/dku-schedule-client/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import { Home } from "../views";
 
 export const router = createRouter({
-  history: createWebHistory("/dku-schdule-manager"),
+  history: createWebHistory("/dku-schedule-manager"),
   routes: [
     {
       path: "/",
